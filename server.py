e836d523587d1f645aea41bbf001cc631b8c708f
kevinpenton <99160838+kevinpenton@users.noreply.github.com>
2022-10-07 16:29:18 -0400

    Update server.py

--- server.py
+++ server.py
@@ -9,11 +9,9 @@
  *                 that this work is my own and that none of it is the work of any
  *                 other person.
  *
- *  Description: The simplified Accio server is a relatively simple application that waits for clients to connect,
- *               accepts a connection, sends the accio\r\n command, afterwards receives confirmation, sends the
- *               accio\r\n command again, receives the second confirmation, and then receives binary file that client
- *               sends,counts the number of bytes received, and prints it out as a single number (number of bytes
- *               received not including the header size).
+ *  Description: The Accio server is an extension of the simplified server that processes multiple simultaneous 
+ *               connections in parallel and saves the received data into the specified folder (following the format 
+ *               1.file, 2.file, etc.)
  *
  *  Usage:  server.py <PORT> <FILE-DIR>
  *==================================================================================================================="""
