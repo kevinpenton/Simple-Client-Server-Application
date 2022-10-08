4cda9e14744286342a0d37b0f0c801b3517660bc
kevinpenton <99160838+kevinpenton@users.noreply.github.com>
2022-10-08 03:59:22 -0400

    Update server.py

--- server.py
+++ server.py
@@ -20,27 +20,20 @@ import threading
 import sys
 import socket
 
-counter = 0
 
-try:
-    host = '0.0.0.0'
-    port = int(sys.argv[1])
-    fileDir = sys.argv[2]
-
+def accept_conn(host, port, conn_fileDir, conn_counter):
     # Creates the socket instance
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         sock.settimeout(10)
 
+    try:
         # Opening listening socket
         sock.bind((host, port))
-        numOfConn = 10
-        sock.listen(numOfConn)
+        sock.listen(1)
 
-
-        def accept_conn(conn_fileDir, conn_counter):
-            try:
-                sock.settimeout(10)
+        try:
+            while True:
                 # Accepts connection
                 clientSocket, clientAddress = sock.accept()
 
@@ -55,8 +48,14 @@ try:
                     # Receives data for the first time
                     bytesRecv1 = clientSocket.recv(1024)
 
-                    #Creates binary file
-                    newFile = open("%s/%s.file" %(conn_fileDir,  conn_counter), 'wb')
+                    # Sends second accio\r\n command
+                    cmd2Send = clientSocket.send(cmd)
+
+                    # Receives data for the second time
+                    bytesRecv2 = clientSocket.recv(1024)
+
+                    # Creates binary file
+                    newFile = open("%s/%s.file" % (conn_fileDir, conn_counter), 'wb')
 
                     while True:
                         recvFile = clientSocket.recv(1024)
@@ -66,24 +65,46 @@ try:
 
                     newFile.close()
 
-            except socket.error:
-                sys.stderr.write("ERROR: Failed to connect to functioning server\n")
-                newFile = open("%s/%s.file" % (conn_fileDir, conn_counter), 'w')
-                newFile.write("ERROR")
-                newFile.close()
+        except socket.error:
+            sys.stderr.write("ERROR: Failed to connect to functioning server\n")
+            newFile = open("%s/%s.file" % (conn_fileDir, conn_counter), 'w')
+            newFile.write("ERROR")
+            newFile.close()
 
+    except socket.error:
+        sys.stderr.write("ERROR: Fail to bind / execute\n")
+        exit(1)
+
+def main():
+    try:
+        host = '0.0.0.0'
+        port = int(sys.argv[1])
+        fileDir = sys.argv[2]
+
+        if port < 1023 or port > 65535:
+            sys.stderr.write("ERROR: Out of range port number\n")
+            exit(1)
+
+    except OverflowError:
+        sys.stderr.write("ERROR: Invalid port number")
+        exit(1)
+
+    try:
+        counter = 1
         threads = []
         # Creates a thread for each connection
-        for _ in range(numOfConn):
-            counter += 1
-            t = threading.Thread(target=accept_conn, args=(fileDir, counter))
+        for _ in range(1,11):
+            t = threading.Thread(target=accept_conn, args=(host, port, fileDir, counter))
             t.start()
             threads.append(t)
+            counter += 1
 
         for thread in threads:
             thread.join()
 
+    except socket.error:
+        sys.stderr.write("ERROR: Failed to connect to functioning server\n")
 
-except OverflowError:
-    sys.stderr.write("ERROR: Invalid port number")
-    exit(1)
+main()
+
+
