a42e76b34a2ba50e7c9d6583462d63cba098c7be
kevinpenton <99160838+kevinpenton@users.noreply.github.com>
2022-10-09 03:40:12 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+
 """=====================================================================================================================
  * Project Info Header
  *================================================================================
@@ -9,107 +10,87 @@
  *                 that this work is my own and that none of it is the work of any
  *                 other person.
  *
- *  Description: The Accio server is an extension of the simplified server that processes multiple simultaneous
- *               connections in parallel and saves the received data into the specified folder (following the format
- *               1.file, 2.file, etc.)
+ *  Description: The simplified Accio server is a relatively simple application that waits for clients to connect,
+ *               accepts a connection, sends the accio\r\n command, afterwards receives confirmation, sends the
+ *               accio\r\n command again, receives the second confirmation, and then receives binary file that client
+ *               sends,counts the number of bytes received, and prints it out as a single number (number of bytes
+ *               received not including the header size).
  *
- *  Usage:  server.py <PORT> <FILE-DIR>
+ *  Usage: server-s.py <PORT>
  *==================================================================================================================="""
 
 import threading
-import socket
 import sys
+import socket
 
 
-def connecting(ipaddress, port, directory, counter):
+try:
+    host = '0.0.0.0'
+    port = int(sys.argv[1])
+
+    # Creates the socket instance
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.settimeout(10)
 
-        try:
-            sock.bind((ipaddress, port))
+        # Opening listening socket
+        sock.bind((host, port))
+        numOfConn = 10
+        sock.listen(numOfConn)
 
-            sock.listen(10)
 
+        # Function to accept a connection and process it accordingly
+        def setConn():
             try:
-                sock.settimeout(10)
+                # Accepts connection
+                clientSocket, clientAddress = sock.accept()
 
-                try:
+                with clientSocket:
+                    # Creates variable for command
+                    cmd = b'accio\r\n'
 
+                    # Send first accio\r\n command
+                    cmd1Send = clientSocket.send(cmd)
+
+                    # Receives data for the first time
+                    bytesRecv1 = clientSocket.recv(1024)
+
+                    # Sends second accio\r\n command
+                    cmd2Send = clientSocket.send(cmd)
+
+                    # Receives data for the second time
+                    bytesRecv2 = clientSocket.recv(1024)
+
+                    # Receives file
+                    l = 0
                     while True:
+                        recvFile = clientSocket.recv(1024)
 
-                        (clientConnection, clientAddress) = sock.accept()
+                        if not recvFile:
+                            break
+                        l += len(recvFile)
 
-                        clientConnection.send(b'accio\r\n')
+                    print(l)
 
-                        recvMsg1 = clientConnection.recv(1024)
-
-                        clientConnection.send(b'accio\r\n')
-
-                        recvMsg2 = clientConnection.recv(1024)
-
-                        # This is where the file gets created and alloted into the file
-
-                        strcounter = str(counter) + ".file"
-                        completeFileName = os.path.join(directory, strcounter)
-                        file = open(completeFileName, 'wb')
-
-                        recvFile = clientConnection.recv(1024)
-
-                        while recvFile:
-                            file.write(recvFile)
-                            recvFile = clientConnection.recv(1024)
-
-                        file.close()
-                        clientConnection.close()
-                        break
-
-                except:
-                    sys.stderr.write("ERROR: (Could not Listen)\n")
-                    exit(1)
-
-            except:
-                strcounter = str(counter) + ".file"
-                completeFileName = os.path.join(directory, strcounter)
-                file = open(completeFileName, 'w')
-                file.write("ERROR")
-                file.close()
-                clientConnection.close()
-                exit(1)
-        except:
-            sys.stderr.write("ERROR: (Could not Bind/Execute)\n")
-            exit(1)
+            except socket.error:
+                sys.stderr.write("ERROR: Failed to connect to functioning server\n")
+                time.sleep(1)
 
 
-def main():
-    try:
-        ipaddress = "0.0.0.0"
-        port = int(sys.argv[1])
-        if port < 1023 or port > 65535:
-            sys.stderr.write("ERROR: (Out of Range Port #)\n")
-            exit(1)
-        directory = sys.argv[2]
+        threads = []
 
+        # Creates a thread for each connection
+        for _ in range(numOfConn):
+            t = threading.Thread(target=setConn)
+            t.start()
+            threads.append(t)
 
-    except:
-        sys.stderr.write("ERROR: (Incorect Type [INTS ONLY])")
-        exit(1)
+        for thread in threads:
+            thread.join()
 
-    try:
-        counter = 1
-
-        for _ in range(1, 11):
-            obj = threading.Thread(target=connecting, args=(ipaddress, port, directory, counter))
-            obj.start()
-            obj.join()
-            counter += 1
-
-
-
-    except:
-        sys.stderr.write("ERROR: (Could not Connect)")
-
-
-main()
+except OverflowError:
+    sys.stderr.write("ERROR: Invalid port number")
+    exit(1)
 
 
 
